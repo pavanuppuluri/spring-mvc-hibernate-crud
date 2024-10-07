18ba21e892ae7f33e5907ec21e533751ac4076e1
Pavan Kumar Uppuluri <63893920+pavanuppuluri@users.noreply.github.com>
2024-10-07 19:10:23 +0530

    Spring MVC Hibernate CRUD

---
+++ src/main/java/springmvc/service/CustomerService.java
@@ -0,0 +1,17 @@
+package springmvc.service;
+
+import java.util.List;
+
+import springmvc.entity.Customer;
+
+public interface CustomerService {
+
+    public List < Customer > getCustomers();
+
+    public void saveCustomer(Customer theCustomer);
+
+    public Customer getCustomer(int theId);
+
+    public void deleteCustomer(int theId);
+
+}
